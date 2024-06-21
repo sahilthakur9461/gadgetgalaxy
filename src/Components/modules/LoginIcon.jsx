85f8435c2ee0116b515678c27330392d2f706103
sahil thakur <st551243@gmail.com>
2024-06-21 09:48:58 +0530

    changes in home

--- src/Components/modules/LoginIcon.jsx
+++ src/Components/modules/LoginIcon.jsx
@@ -12,23 +12,7 @@ function LoginIcon() {
 
   return (
     <div className="relative">
-      {/* {!isLoggedIn ? (
-        <a href="/" onClick={handleLoginClick}>
-          <img
-            src={login}
-            alt="login"
-            className="w-12 cursor-pointer absolute right-0  transition-all duration-300"
-          />
-        </a>
-      ) : (
-        <a href="/" onClick={handleLogoutClick}>
-          <img
-            src={logout}
-            alt="logout"
-            className="w-12 cursor-pointer transition-all duration-300"
-          />
-        </a>
-      )} */}
+  
       {isLoggedIn  ? <CgLogIn color="white" size={25} className="cursor-pointer" onClick={handleLoginClick}/> : <CgLogOut color="white" size={25} className="cursor-pointer" onClick={handleLoginClick}/>}
     </div>
   );
